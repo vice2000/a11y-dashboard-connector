f45589593ebbd5f413782e4ca3cfcdab98c9cb27
Valentin von Guttenberg <greengiraffe@posteo.de>
2018-03-01 16:43:12 +0100

    Add function to get top issues per guideline

--- src/reporter.js
+++ src/reporter.js
@@ -1,8 +1,11 @@
 exports = module.exports = {}
 
-exports.supports = '^5.0.0'
-
-// Called with the results of a test run
+/**
+ * Get the error, notice and warning statistics for a given pa11y result
+ *
+ * @param {object} The pa11y results
+ * @returns {object} Object containing errorCount, warningCount and noticeCount
+ */
 exports.results = results => {
   return {
     errorCount: results.issues.filter(issue => issue.type === 'error').length,
@@ -11,7 +14,30 @@ exports.results = results => {
   }
 }
 
-// begin(); // Called when pa11y starts
-// error(message); // Called when a technical error is reported
-// debug(message); // Called when a debug message is reported
-// info(message); // Called when an information message is reported
+/**
+ * Get the top n guidelines with the most issues (warning, error or notice)
+ * for the given pa11y results.
+ *
+ * @param {object} results The pa11y results
+ * @param {number} count Maximum number of guidelines to return
+ * @returns {Map} A sorted map of the top n issues
+ */
+exports.topIssuesPerGuideline = (results, top) => {
+  const guidelineMap = new Map()
+
+  results.issues.forEach(issue => {
+    // Transform code string to array, for example:
+    // Input: 'WCAG2AA.Principle2.Guideline2_4.2_4_1.H64.1'
+    // Result: [ 'WCAG2AA', 'Principle2', 'Guideline2_4', '2_4_1', 'H64', '1' ]
+    const splittedCodes = issue.code.match(/([^.]+)/g)
+    const guideline = splittedCodes[2]
+
+    guidelineMap.set(guideline, guidelineMap.get(guideline) + 1 || 1)
+  })
+
+  const sortedGuidelines = [...guidelineMap.entries()].sort((a, b) => {
+    return a[1] < b[1] // sort by guidline occurence
+  })
+
+  return new Map(sortedGuidelines.slice(0, top))
+}
